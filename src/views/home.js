842d2a4654bbd913d59cdca3fec515dcc76bdff6
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-08-18 20:46:04 -0500

    checkbox colors turn on and off
    custom styling for checkbox achieved by hiding checkbox and styling the label that is for it
    then we added css to change the colours of the text based on whether the checkbox was checked or not (using adjacent sibling selectors)
    also used state to change the color of the svg

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Device from "./device";
 const Home = (props) => {
-  return <div>Home Screen </div>;
+  return (
+    <div>
+      <p>Home Screen</p>
+      <Device deviceName="Air Conditioner" />
+    </div>
+  );
 };
 export default Home;
